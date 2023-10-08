7b37d3c78c79e87dc153cc6b02f21a34d3154689
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2023-10-08 10:00:10 -0300

    Finite State Machine introduced

---
+++ BehavioralPattern/FiniteStateMachineExample/FiniteStateMachine/FSMState.cs
@@ -0,0 +1,13 @@
+﻿namespace FiniteStateMachineExample.FiniteStateMachine
+{
+    public enum FSMState
+    {      
+        Success = 0,
+        Initializing = 1,
+        Processing = 2,
+        Finishing = 3,
+        ErrorAtInitializing = 11,
+        ErrorAtProcessing = 12,
+        ErrorAtFinishing = 13,
+    }    
+}
